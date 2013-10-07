d782ec3829ed8a149a740633fef1635ae3f3ec2c
CosmicDan <cosmicdan@vmware-ubuntu.(none)>
2013-10-07 15:01:45 +1100

    Use stock AtciService.apk

--- customize_target_files.sh
+++ customize_target_files.sh
@@ -1,6 +1,7 @@
 # These custom commands are run before the final ZIP is produced
 
 rm ./out/target_files/DATA/preinstall_apps/AlipayMsp.apk
+rm ./out/target_files/DATA/preinstall_apps/AtciService.apk
 rm ./out/target_files/DATA/preinstall_apps/MiShop.apk
 rm ./out/target_files/SYSTEM/app/BugReport.apk
 rm ./out/target_files/SYSTEM/app/LBESEC_MIUI.apk
