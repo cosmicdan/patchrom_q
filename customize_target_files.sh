86a0e27a7b47dcf42d02073e602785dc0ca70fe1
CosmicDan <cosmicdan@vmware-ubuntu.(none)>
2013-10-12 17:40:29 +1100

    Remove VoiceAssist (chinese) and QuickSearchBox (bugged)

--- customize_target_files.sh
+++ customize_target_files.sh
@@ -3,7 +3,9 @@
 rm ./out/target_files/DATA/preinstall_apps/AlipayMsp.apk
 rm ./out/target_files/DATA/preinstall_apps/AtciService.apk
 rm ./out/target_files/DATA/preinstall_apps/MiShop.apk
+rm ./out/target_files/DATA/preinstall_apps/VoiceAssist.apk
 rm ./out/target_files/SYSTEM/app/BugReport.apk
+rm ./out/target_files/SYSTEM/app/QuickSearchBox.apk
 rm ./out/target_files/SYSTEM/app/SuperMarket.apk
 rm ./out/target_files/SYSTEM/app/Updater.apk
 exit 0
